40e86188b0c96d1292015a1e06fe973b76f2940a
MegaLiquida <marcos@megaliquidasp.com.br>
2026-04-01 23:54:41 -0300

    Delete drizzle directory

--- drizzle/0003_smart_lifeguard.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `ncm_cache` ADD CONSTRAINT `ncm_cache_account_product_idx` UNIQUE(`accountId`,`productId`);
--- drizzle/0004_luxuriant_leper_queen.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `ncm_cache` DROP INDEX `ncm_cache_account_product_idx`;--> statement-breakpoint
-ALTER TABLE `ncm_cache` MODIFY COLUMN `productId` int;--> statement-breakpoint
-ALTER TABLE `ncm_cache` ADD CONSTRAINT `ncm_cache_account_sku_idx` UNIQUE(`accountId`,`sku`);
